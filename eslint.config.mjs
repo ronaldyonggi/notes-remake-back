9ddffbee18b5c0dec7114c9c63759d3cd42e3530
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-09 15:14:19 +0800

    configure granular option for no-misused-promises

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,5 +27,6 @@ export default tseslint.config({
     '@typescript-eslint/restrict-template-expressions': 'off',
     '@typescript-eslint/restrict-plus-operands': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-misused-promises': [2, { checksVoidReturn: false }],
   },
 });
