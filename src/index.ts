81f6ae15b6f2be98480aa89c40e23cc7736f7b01
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-10 23:29:17 +0800

    add step to test db connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import app from './app';
 import config from './utils/config';
+import { connectToDatabase } from './utils/db';
 
-app.listen(config.PORT, () => {
-  console.log(`Server is running on port ${config.PORT}`);
-});
+const start = async () => {
+  await connectToDatabase();
+  app.listen(config.PORT, () => {
+    console.log(`Server is running on port ${config.PORT}`);
+  });
+};
+
+void start();
