cc19396ef405b457fc0c4fb2ebc6f23f85f3f9a3
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-10 23:29:57 +0800

    add endpoint for ping

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,12 @@ import notesRouter from './controllers/notes';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());;
+
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
+});
 
 app.use('/notes', notesRouter);
 
