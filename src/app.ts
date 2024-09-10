54293ee824005f261d818e461b4f79eccad8b927
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-10 10:32:14 +0800

    use notes router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import 'express-async-errors';
 import cors from 'cors';
+import notesRouter from './controllers/notes';
 
 // Initialize express app
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/notes', notesRouter);
 
 export default app;
