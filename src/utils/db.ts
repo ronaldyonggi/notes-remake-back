59fc528a1ab45612bae9a6a315c24efed027fcaa
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-09-11 14:17:31 +0800

    refactor variable names and PG host selection

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,11 +1,14 @@
 import { Client } from 'pg';
-const POSTGRES_USER = process.env.POSTGRES_USER;
-const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
-const POSTGRES_DB = process.env.POSTGRES_DB;
+const pgUser = process.env.POSTGRES_USER;
+const pgPassword = process.env.POSTGRES_PASSWORD;
+const pgDb = process.env.POSTGRES_DB;
+const isUsingDocker = process.env.DOCKER;
+const pgLocalHost = process.env.POSTGRES_HOST_LOCAL;
+const pgDockerHost = process.env.POSTGRES_HOST_DOCKER;
 // If the environment is started using docker compose, use the docker host instead of the localhost
-const POSTGRES_HOST = process.env.DOCKER ? process.env.POSTGRES_HOST_DOCKER : process.env.POSTGRES_HOST;
+const pgHostRun = isUsingDocker ? pgDockerHost : pgLocalHost;
 
-const connectionString = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:5432/${POSTGRES_DB}`;
+const connectionString = `postgresql://${pgUser}:${pgPassword}@${pgHostRun}:5432/${pgDb}`;
 
 const client = new Client({
   connectionString,
